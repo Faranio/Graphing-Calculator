65dcd9ef207ce0a4bad9e1670977fafef3e71024
Faranio <agent006_99@mail.ru>
2021-08-06 14:13:05 +0600

    Day 2

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+from math import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def get_input():
 	equation = str(input())
+	return equation
+
+
+def draw_function(equation):
+	x_values = np.linspace(-10, 11, 1000)
+	y_values = []
+	
+	for x in x_values:
+		y = eval(equation)
+		y_values.append(y)
+		
+	plt.plot(x_values, y_values)
+	plt.show()
 
 
 def main():
-	get_input()
+	equation = get_input()
+	draw_function(equation)
 	
 
 if __name__ == "__main__":
