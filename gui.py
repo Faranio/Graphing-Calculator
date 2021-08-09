9f3096b40244315e4b3bf793a8bceab0dff5e64a
Faranio <agent006_99@mail.ru>
2021-08-09 14:37:29 +0600

    Day 5

--- gui.py
+++ gui.py
@@ -1,6 +1,35 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from math import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
 
-from graph import *
+X_RANGE = [-10, 11]
+Y_RANGE = []
+
+
+def draw_function(function):
+	global Y_RANGE
+	plt.clf()
+	x_values = np.linspace(*X_RANGE, 1000)
+	y_values = []
+	
+	for x in x_values:
+		y = eval(function)
+		y_values.append(y)
+	
+	Y_RANGE.append(min(y_values))
+	Y_RANGE.append(max(y_values))
+	
+	plt.xlim(*X_RANGE)
+	plt.ylim(*Y_RANGE)
+	plt.grid()
+	plt.plot(x_values, y_values)
+	fig.canvas.draw()
+	
+	Y_RANGE = []
 
 
 def graph():
@@ -8,8 +37,15 @@ def graph():
 	draw_function(equation)
 
 
+matplotlib.use('TkAgg')
+fig = plt.figure(1)
+
 window = Tk()
 window.title('Graphing Calculator')
+
+canvas = FigureCanvasTkAgg(fig, master=window)
+plot_widget = canvas.get_tk_widget()
+plot_widget.pack()
 
 Label(window, text='Type equation here:').pack()
 
